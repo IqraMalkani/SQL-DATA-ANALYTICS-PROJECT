7da21282329a1774ea5331dd929294fe05e12f0b
Iqra Malkani <hello@iqramalkani.com>
2026-02-26 11:00:04 -0500

    Update 00_init_database.sql

--- scripts/00_init_database.sql
+++ scripts/00_init_database.sql
@@ -82,7 +82,7 @@ TRUNCATE TABLE gold.dim_customers;
 GO
 
 BULK INSERT gold.dim_customers
-FROM 'C:\sql\sql-data-analytics-project\datasets\csv-files\gold.dim_customers.csv'
+FROM 'D:\Downloads\sql-data-analytics-project\datasets\csv-files\gold.dim_customers.csv'
 WITH (
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
@@ -94,7 +94,7 @@ TRUNCATE TABLE gold.dim_products;
 GO
 
 BULK INSERT gold.dim_products
-FROM 'C:\sql\sql-data-analytics-project\datasets\csv-files\gold.dim_products.csv'
+FROM 'D:\Downloads\sql-data-analytics-project\datasets\csv-files\gold.dim_products.csv'
 WITH (
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
@@ -106,7 +106,7 @@ TRUNCATE TABLE gold.fact_sales;
 GO
 
 BULK INSERT gold.fact_sales
-FROM 'C:\sql\sql-data-analytics-project\datasets\csv-files\gold.fact_sales.csv'
+FROM 'D:\Downloads\sql-data-analytics-project\datasets\csv-files\gold.fact_sales.csv'
 WITH (
 	FIRSTROW = 2,
 	FIELDTERMINATOR = ',',
